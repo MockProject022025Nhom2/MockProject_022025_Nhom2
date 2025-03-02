17f471c4b4cc4443af96428532b01bd983ee6381
Đỗ Quốc Phong <87427739+Babychandoi@users.noreply.github.com>
2025-03-02 23:16:26 +0700

    login

---
+++ MockProject/src/main/java/org/example/mockproject/dto/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package org.example.mockproject.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AuthenticationResponse {
+    String token;
+    boolean authenticated;
+}
---
+++ MockProject/src/main/java/org/example/mockproject/entity/User.java
@@ -0,0 +1,31 @@
+package org.example.mockproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+    private String gender;
+    private String address;
+    private String role;
+    @CreationTimestamp
+    private Date createdDate;
+}
